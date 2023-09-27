7c0af98de810ec05c9d32db2f48d233e65b6ed65
ryhnslan <ryhnslan@gmail.com>
2023-09-27 22:49:29 +0300

    iletişim sayfası taslak

--- src/examples/Cards/BlogCards/BackgroundBlogCard/index.js
+++ src/examples/Cards/BlogCards/BackgroundBlogCard/index.js
@@ -1,33 +1,33 @@
 // react-router components
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
 // @mui material components
 import Card from "@mui/material/Card";
-import MuiLink from "@mui/material/Link";
-import Icon from "@mui/material/Icon";
+// import MuiLink from "@mui/material/Link";
+// import Icon from "@mui/material/Icon";
 
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 
-function BackgroundBlogCard({ image, title, description, action }) {
-  const cardActionStyles = {
-    display: "flex",
-    alignItems: "center",
-    width: "max-content",
+function BackgroundBlogCard({ image, title, description }) {
+  // const cardActionStyles = {
+  //   display: "flex",
+  //   alignItems: "center",
+  //   width: "max-content",
 
-    "& .material-icons, .material-icons-round,": {
-      transform: `translateX(2px)`,
-      transition: "transform 0.2s cubic-bezier(0.34,1.61,0.7,1.3)",
-    },
+  //   "& .material-icons, .material-icons-round,": {
+  //     transform: `translateX(2px)`,
+  //     transition: "transform 0.2s cubic-bezier(0.34,1.61,0.7,1.3)",
+  //   },
 
-    "&:hover .material-icons, &:focus .material-icons, &:hover .material-icons-round, &:focus .material-icons-round":
-      {
-        transform: `translateX(6px)`,
-      },
-  };
+  //   "&:hover .material-icons, &:focus .material-icons, &:hover .material-icons-round, &:focus .material-icons-round":
+  //     {
+  //       transform: `translateX(6px)`,
+  //     },
+  // };
 
   return (
     <Card
@@ -54,7 +54,7 @@ function BackgroundBlogCard({ image, title, description, action }) {
           <MKTypography variant="body2" color="white" my={3}>
             {description}
           </MKTypography>
-          {action.type === "internal" ? (
+          {/* {action.type === "internal" ? (
             <MKTypography
               component={Link}
               to={action.route}
@@ -82,7 +82,7 @@ function BackgroundBlogCard({ image, title, description, action }) {
               {action.label}
               <Icon sx={{ fontWeight: "bold" }}>arrow_forward</Icon>
             </MKTypography>
-          )}
+          )} */}
         </MKBox>
       </MKBox>
     </Card>
--- src/pages/LandingPages/ContactUs/index.js
+++ src/pages/LandingPages/ContactUs/index.js
@@ -16,23 +16,29 @@ import footerRoutes from "footer.routes";
 
 // Image
 // import bgImage from "assets/images/illustrations/illustration-reset.jpg";
-const cardContainerStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "flex-start",
-};
+// const cardContainerStyle = {
+//   display: "flex",
+//   justifyContent: "space-between",
+//   alignItems: "flex-start",
+// };
 
 const cardStyle = {
-  width: "1000px",
-  height: "200px",
+  width: "600px",
+  height: "150px",
   padding: "1rem",
-  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
   borderRadius: "8px",
   marginBottom: "1rem",
-  marginRight: "1rem",
+  textAlign: "center",
+  color: "black",
 };
 
 function ContactUs() {
+  const email =
+    "https://img.freepik.com/free-vector/envelope-mail-set_271628-376.jpg?w=1380&t=st=1695843032~exp=1695843632~hmac=66cd42de91d33fb0d67caa3d7edd2750815cbfd4e61f92eec84cce7ebbb1314e";
+  const adres = "https://cdn.pixabay.com/photo/2018/04/12/18/13/marker-3314279_1280.png";
+  const phone =
+    "https://img.freepik.com/free-photo/top-view-blue-monday-concept-composition-with-telephone_23-2149139106.jpg?w=1380&t=st=1695842013~exp=1695842613~hmac=bbe8d7b00c95f26157f7c4875836bedd0f7c5de0c8ce47128eef4fbc1f81ea77";
   return (
     <>
       <MKBox position="fixed" top="0.5rem" width="100%">
@@ -47,163 +53,197 @@ function ContactUs() {
           }}
         />
       </MKBox>
+      <Grid container spacing={3} alignItems="center" marginTop={"8rem"} marginLeft={"1rem"}>
+        <Grid item xs={12} lg={5}>
+          <Grid container spacing={3} alignItems="center">
+            <Grid item xs={12} lg={12} marginLeft={"10rem"}>
+              <Grid container spacing={2}>
+                <Grid marginBottom={"2rem"} item xs={12} sm={4}>
+                  <Card
+                    sx={{
+                      backgroundImage: ({
+                        palette: { black },
+                        functions: { linearGradient, rgba },
+                      }) =>
+                        `${linearGradient(
+                          rgba(black.main, 0.1),
+                          rgba(black.main, 0.5)
+                        )}, url(${adres})`,
+                      backgroundSize: "cover",
+                    }}
+                    style={cardStyle}
+                  >
+                    <CardContent>
+                      <MKTypography variant="h5">Adres</MKTypography>
+                      <p>Sokullu Mehmet Paşa, 1361. Sk. No:6 D:25, 06420 Çankaya/Ankara</p>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+              <Grid container spacing={2}>
+                <Grid marginBottom={"2rem"} item xs={12} sm={4}>
+                  <Card
+                    sx={{
+                      backgroundImage: ({
+                        palette: { black },
+                        functions: { linearGradient, rgba },
+                      }) =>
+                        `${linearGradient(
+                          rgba(black.main, 0.1),
+                          rgba(black.main, 0.5)
+                        )}, url(${phone})`,
+                      backgroundSize: "cover",
+                    }}
+                    style={cardStyle}
+                  >
+                    <CardContent>
+                      <MKTypography variant="h5">Telefon</MKTypography>
+                      <p>+90 312 433 27 35</p>
+                      <p>+90 507 868 89 53</p>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+              <Grid container spacing={2}>
+                <Grid marginBottom={"2rem"} item xs={12} sm={4}>
+                  <Card
+                    sx={{
+                      backgroundImage: ({
+                        palette: { black },
+                        functions: { linearGradient, rgba },
+                      }) =>
+                        `${linearGradient(
+                          rgba(black.main, 0.1),
+                          rgba(black.main, 0.5)
+                        )}, url(${email})`,
+                      backgroundSize: "cover",
+                    }}
+                    style={cardStyle}
+                  >
+                    <CardContent>
+                      <MKTypography variant="h5">Mail</MKTypography>
+                      <p>arenadis@gmail.com</p>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} lg={6}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <MKBox
+                bgColor="white"
+                borderRadius="xl"
+                shadow="lg"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                mx={3}
+              >
+                <MKInput
+                  size="large"
+                  type="text"
+                  variant="outlined"
+                  label="Adınız Soyadınız"
+                  InputLabelProps={{ shrink: true }}
+                />
+              </MKBox>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <MKBox
+                bgColor="white"
+                borderRadius="xl"
+                shadow="lg"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                mx={3}
+              >
+                <MKInput
+                  type="email"
+                  variant="outlined"
+                  label="Email adresiniz"
+                  InputLabelProps={{ shrink: true }}
+                  fullWidth
+                />
+              </MKBox>
+            </Grid>
+            <Grid item xs={12}>
+              <MKBox
+                bgColor="white"
+                borderRadius="xl"
+                shadow="lg"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                mx={3}
+              >
+                <MKInput
+                  variant="outlined"
+                  type="text"
+                  label="Nasıl yardımcı olabiliriz?"
+                  placeholder="Describe your problem in at least 250 characters"
+                  InputLabelProps={{ shrink: true }}
+                  multiline
+                  fullWidth
+                  rows={8}
+                />
+              </MKBox>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="flex-end" xs={11} mt={3} mb={2}>
+            <MKButton type="submit" variant="gradient" color="info">
+              Gönder
+            </MKButton>
+          </Grid>
+        </Grid>
+      </Grid>
       <Grid
         item
-        xs={8}
-        sm={8}
-        md={7}
-        lg={6}
-        xl={4}
-        ml={{ xs: "auto", lg: 6 }}
-        mr={{ xs: "auto", lg: 6 }}
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        xl={12}
+        ml={{ xs: "auto", lg: 12 }}
+        mr={{ xs: "auto", lg: 12 }}
       >
-        <MKBox
-          bgColor="white"
-          borderRadius="xl"
-          shadow="lg"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          mt={{ xs: 20, sm: 18, md: 20 }}
-          mb={{ xs: 20, sm: 18, md: 20 }}
-          mx={3}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "90%",
+            height: "calc(60vh - 5rem)",
+            margin: "0 auto",
+            borderRadius: "20px",
+            overflow: "hidden",
+          }}
         >
-          {" "}
-          <Grid
-            item
-            xs={8}
-            sm={8}
-            md={7}
-            lg={6}
-            xl={4}
-            ml={{ xs: "auto", lg: 6 }}
-            mr={{ xs: "auto", lg: 6 }}
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+              borderRadius: "20px",
+              overflow: "hidden",
+            }}
           >
-            <MKTypography variant="h3" color="white">
-              Bizimle İletişime Geçin ggfgg
-            </MKTypography>{" "}
-          </Grid>
-        </MKBox>
-        <MKBox p={10}>
-          <MKBox width="100%" component="form" method="post" autoComplete="off">
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={4}>
-                <MKBox
-                  bgColor="white"
-                  borderRadius="xl"
-                  shadow="lg"
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  mx={3}
-                >
-                  <MKInput
-                    variant="standard"
-                    label="Adınız"
-                    InputLabelProps={{ shrink: true }}
-                    fullWidth
-                  />
-                </MKBox>
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <MKBox
-                  bgColor="white"
-                  borderRadius="xl"
-                  shadow="lg"
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  mx={3}
-                >
-                  <MKInput
-                    type="email"
-                    variant="standard"
-                    label="Email adresiniz"
-                    InputLabelProps={{ shrink: true }}
-                    fullWidth
-                  />{" "}
-                </MKBox>
-              </Grid>
-              <Grid item xs={8}>
-                <MKBox
-                  bgColor="white"
-                  borderRadius="xl"
-                  shadow="lg"
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  mx={3}
-                >
-                  <MKInput
-                    variant="standard"
-                    label="Nasıl yardımcı olabiliriz?"
-                    placeholder="Describe your problem in at least 250 characters"
-                    InputLabelProps={{ shrink: true }}
-                    multiline
-                    fullWidth
-                    rows={6}
-                  />
-                </MKBox>
-              </Grid>
-            </Grid>
-            <Grid container item justifyContent="center" xs={12} mt={5} mb={2}>
-              <MKButton type="submit" variant="gradient" color="info">
-                Gönder
-              </MKButton>
-            </Grid>
-          </MKBox>
-        </MKBox>
-      </Grid>
-      <Grid container spacing={5} alignItems="center" marginTop={"5rem"} marginLeft={"5rem"}>
-        <Grid item xs={12} lg={6}>
-          <Grid container spacing={3} alignItems="center">
-            <Grid item xs={12} lg={12} marginLeft={"30rem"}>
-              <div style={cardContainerStyle}>
-                <Card style={cardStyle}>
-                  <CardContent>
-                    <MKTypography variant="h5">Adres</MKTypography>
-                    <p>Sokullu Mehmet Paşa, 1361. Sk. No:6 D:25, 06420 Çankaya/Ankara</p>
-                  </CardContent>
-                </Card>
-                <Card style={cardStyle}>
-                  <CardContent>
-                    <MKTypography variant="h5">Telefon</MKTypography>
-                    <p>+90 312 433 27 35</p>
-                    <p>+90 507 868 89 53</p>
-                  </CardContent>
-                </Card>
-                <Card style={cardStyle}>
-                  <CardContent>
-                    <MKTypography variant="h5">Email</MKTypography>
-                    <p>info@arenadis.com.tr</p>
-                  </CardContent>
-                </Card>
-              </div>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-              xl={12}
-              ml={{ xs: "auto", lg: 12 }}
-              mr={{ xs: "auto", lg: 12 }}
-            ></Grid>
             <iframe
               style={{
                 width: "100%",
-                height: "calc(60vh - 5rem)",
-                borderRadius: "lg",
-                // marginLeft: "10rem",
-                marginTop: "1rem",
+                height: "100%",
+                borderRadius: "20px",
               }}
               src={
                 "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4299.443701551748!2d32.8357730760411!3d39.8896592975134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f8df2ae40e3%3A0x5be6915b99fe5ed7!2zQVJFTkEgRMSwxZ4gREVQT1NVIEEuxZ4u!5e0!3m2!1str!2str!4v1671102703358!5m2!1str!2str"
               }
-            ></iframe>{" "}
-          </Grid>
-        </Grid>
+            ></iframe>
+          </div>
+        </div>
       </Grid>
       <MKBox pt={6} px={1} mt={6}>
         <DefaultFooter content={footerRoutes} />
