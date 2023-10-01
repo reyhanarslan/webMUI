f3dbeeebefa3bfe84ae8007cb03340ee228a605b
ryhnslan <ryhnslan@gmail.com>
2023-10-01 11:14:35 +0300

    product detail page

--- src/products/AboutUs/index.js
+++ src/products/AboutUs/index.js
@@ -6,7 +6,6 @@ import Card from "@mui/material/Card";
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
-
 // About Us page sections
 import Information from "pages/LandingPages/AboutUs/sections/Information";
 import "./product.css";
@@ -23,8 +22,8 @@ import routes from "routes";
 import PropTypes from "prop-types";
 
 // Images
-import bgImage from "assets/images/bg-about-us.jpg";
-import { useNavigate, useParams } from "react-router-dom";
+// import bgImage from "assets/images/bg-about-us.jpg";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button, CardMedia } from "@mui/material";
 import { useEffect, useState } from "react";
 import Products from "products/ProductDatas/products";
@@ -36,8 +35,8 @@ export function ProductSlider({ images }) {
       className="d-flex justify-content-center align-items-center"
       animation="slide"
       duration={500}
-      width={1000}
-      height={500}
+      width={600}
+      height={400}
       navButtnsAlwaysVisible={false}
       fullHeightHover={true}
       stopAutoPlayOnHover={true}
@@ -47,8 +46,8 @@ export function ProductSlider({ images }) {
         <CardMedia
           className="d-flex justify-content-center align-items-center carousel-item"
           style={{
-            width: 1000,
-            height: 500,
+            width: 600,
+            height: 400,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
@@ -72,9 +71,11 @@ function ProductInfo() {
   const navigate = useNavigate();
   const { productid } = useParams();
   const [productInfo, setProductInfo] = useState({});
+  const [productImages, setProductImages] = useState([]);
   const test = () => {
     navigate("/urunler/agiz-ici-tarayicilar/:asdasd");
   };
+
   useEffect(() => {
     console.log(productid);
     let result = productid.replace(":", "");
@@ -85,6 +86,8 @@ function ProductInfo() {
         console.log(section.id);
         if (result === section.id) {
           setProductInfo(section);
+          setProductImages(section.img);
+          console.log(section);
         }
       });
     });
@@ -127,7 +130,16 @@ function ProductInfo() {
             >
               {" "}
             </MKBox> */}
-            <ProductSlider images={[bgImage, bgImage, bgImage]} />
+            {/* <div>
+              {Products.map((product) => (
+                <div key={product.index}>
+                  {Products.product.map((item) => (
+                    <ProductSlider key={item.id} images={item.img} />
+                  ))}
+                </div>
+              ))}
+            </div> */}
+            <ProductSlider images={productImages} />
           </Grid>
           <Grid item xs={12} md={4}>
             <MKBox
@@ -148,18 +160,25 @@ function ProductInfo() {
                 sx={{ mx: "auto", textAlign: "center" }}
               >
                 <MKTypography
+                  className="productTitle"
                   variant="h1"
                   color="white"
-                  sx={({ breakpoints, typography: { size } }) => ({
-                    [breakpoints.down("md")]: {
-                      fontSize: size["3xl"],
-                    },
-                  })}
+                  // sx={({ breakpoints, typography: { size } }) => ({
+                  //   [breakpoints.down("md")]: {
+                  //     fontSize: size["3xl"],
+                  //   },
+                  // })}
                 >
-                  Urunler Title
                   {productInfo.model}
                 </MKTypography>
-                <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
+                <MKTypography
+                  className="productSubtitle"
+                  variant="body1"
+                  color="white"
+                  opacity={0.8}
+                  mt={1}
+                  mb={3}
+                >
                   Urunler SubTitle
                   {productid} nolu id
                 </MKTypography>
@@ -172,9 +191,11 @@ function ProductInfo() {
                   Daha Fazla Bilgi İçin Bize Ulaşın
                 </MKTypography>
                 <MKBox display="flex" justifyContent="center" alignItems="center">
-                  <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                    <i className="fa fa-whatsapp" />
-                  </MKTypography>
+                  <Link to="https://api.whatsapp.com/send?phone=+905078688953&text=Merhabasiparivermekistiyorum">
+                    <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+                      <i className="fa fa-whatsapp" />
+                    </MKTypography>
+                  </Link>
                   <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
                     <i className="fa fa-phone" />
                   </MKTypography>
