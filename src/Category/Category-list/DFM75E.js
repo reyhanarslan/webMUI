952c14cea0b00fdad3272c8e0d1472d953fef7fc
EmreCandemir <emre.candemir.h@gmail.com>
2023-10-08 14:09:20 +0300

    product components

---
+++ src/Category/Category-list/DFM75E.js
@@ -0,0 +1,43 @@
+import React from "react";
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import MKBox from "components/MKBox";
+// import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
+import CenteredBlogCard from "examples/Cards/BlogCards/CenteredBlogCard";
+
+function DFM75E() {
+  return (
+    <div>
+      <MKBox component="section" py={12}>
+        <Container>
+          <Grid container spacing={3} alignItems="center">
+            <Grid item xs={12} lg={8}>
+              {/* Başlık */}
+              <div className="page-title"></div>
+
+              <div className="page-content"></div>
+
+              {/* ... */}
+            </Grid>
+            <Grid item xs={12} lg={4} sx={{ ml: "auto", mt: { xs: 3, lg: 0 } }}>
+              <CenteredBlogCard
+                image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                title="Get insights on Search"
+                description="Website visitors today demand a frictionless user expericence — especially when using search. Because of the hight standards."
+                action={{
+                  type: "internal",
+                  route: "pages/company/about-us",
+                  color: "info",
+                  label: "find out more",
+                }}
+              />{" "}
+            </Grid>
+          </Grid>
+        </Container>
+      </MKBox>
+    </div>
+  );
+}
+
+export default DFM75E;
