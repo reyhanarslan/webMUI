3e255b3d2167c4d6c2050c772725d4cafaa1d7df
EmreCandemir <emre.candemir.h@gmail.com>
2023-10-08 13:38:08 +0300

    3700 text

--- src/Category/Category-list/carestream3700.js
+++ src/Category/Category-list/carestream3700.js
@@ -25,18 +25,6 @@ function Carestream3700() {
                 Restoratif,Ortodontik ,İmplant klink uygulamaları ve hatta cihaz uyku modu arasında
                 kullanıcının kontrolünde olan devrimsel bir iş akışı özgürlüğü sağlar.
               </div>
-              <div className="page-title">ÖĞÜRME REFLEKSİ OLAN HASTALARIN RAHATSIZLIĞINA SON</div>
-              <div className="page-content">
-                Belirgin bir öğürme refleksi diş tedavisinin gerçekleştirilmesi için potansiyel bir
-                problemdir. Bu açıdan bir dizi strateji mevcut olsa bile, en basit prosedürlerin
-                bile kabul görmediği hastalar vardır. Bu durum, tedavi planı ve sonucu üzerinde
-                önemli bir etki yaratabilir. Bu sorun ağız içi tarama sistemiyle birlikte tamamen
-                sona eriyor! Bununla birlikte ağız içi tarayıcılar; azaltılmış hasta rahatsızlığı,
-                zaman verimliliği, basitleştirilmiş klinik prosedürleri ve ölçü malzemelerine gerek
-                kalmadan yüksek doğruluğa sahip taramalar yakalama ve bunları kolay depolama imkanı
-                gibi birçok avantaj sunuyor. Bir diğer avantaj ise, hemen görselleştirilebilen
-                tarama veri sonuçlarının teknisyene e-posta yoluyla kolayca ulaştırılabilmesi.
-              </div>
               <div className="page-title">Ergonomik Kavrama Tasarımı</div>
               <div className="page-content">
                 Ergonomik olarak en uygun tutuş kavrama tasarımı ile , Diş hekmilerine dengeli bir
@@ -47,7 +35,7 @@ function Carestream3700() {
                 Dokunmatik ekranın özelliği ;tarayıcı ve mouse arasında ileri/geri geçişin neden
                 olduğu ek işlem adımlarını ortadan kaldırır.
               </div>
-              <div className="page-title">Arttırılmış Hassasiyet</div>
+              <div className="page-title">Artırılmış Hassasiyet</div>
               <div className="page-content">
                 Test edilmiş ve onaylanmış CS3700’ün ultra hızlı tarama özelliği ile alt yada üst
                 çeneyi 30 sn içinde CS 3600 ile aynı netlikte tarayın.
@@ -56,6 +44,61 @@ function Carestream3700() {
               <div className="page-content">
                 Opsiyonel bilek kayışı,kullanıcıya tedavi sırasında cihazı en iyi şekilde kontrol
                 etmesini sağlar ve tarayıcının elden düşme riskini engeller.
+              </div>
+              <div className="page-title">Smart-Shade Teknolojisi</div>
+              <div className="page-content">
+                Tek yüzeyden/yönden renk tonlarını toplayan diğer sistemlerden farklı olarak; CS3700
+                yüzey üzerindeki bir çok farklı noktadan renk tonlarını toplar böylelikle daha net
+                ve doğru bir klinik eşleme yapmamızı sağlar.
+              </div>
+              <div className="page-title">Smart Shade Eşleşme Raporu</div>
+              <div className="page-content">
+                Tek bir tıklama ile çalışılan bölge üzerindeki diş tonlarına ait raporu hızlıca
+                oluşturabilir,seçilmiş ilgi alanı için oluşturulan bu raporu , kullanıcıların
+                çalıştıkları diş laboratuarlarıyla kolayca paylaşmalarını ve yönlendirmelerini
+                sağlar.
+              </div>
+              <div className="page-title">Kalibre Edilmiş Parlaklık</div>
+              <div className="page-content">
+                Kutu açılışında CS 3700 en güvenilir sabit ton eşlemesi için önceden kalibre
+                edilerek kullancılarına gönderilmiştir.50 saatlik kullanım sonrasında ,akıllı
+                kalibrasyon özelliği sayesinde cihaz sizi uyarıyor ve 12 saniyelik bir süre içinde
+                gri bir kart üzerinde kullanıcılarına yeni kalibrasyonu yapmalarına olanak sağlıyor.
+              </div>
+              <div className="page-title">Çalışma Akışınız Hiç Bu Kadar Özgür Olmamıştı!</div>
+              <div className="page-content">
+                CS ScanFlow yazılımı intraoral tarama yazılım dünyasını yeniden inşa ediyor. Diğer
+                tarama yazılım programlarının aksine tamamen açık olması kullanıcıya, sınırsız iş
+                akışı kapasitesine sahip farklı klinik uygulamalar arasında özgürce, genişletilmiş
+                işlem seçenekleri, tek tıklamayla dışa aktarma ve dokunmatik ekran özellikleri
+                sağlamaktadır.
+              </div>
+              <div className="page-title">Sınırsız İş Akışı Kapasitesi</div>
+              <div className="page-content">
+                Tamamen yenilenmiş iş akış tasarımı; kullanıcıların tek bir taramadan farklı iş
+                akışları arasında serbestçe gezinmelerini sağlar.
+              </div>
+              <div className="page-title">Genişletilmiş Data İşleme</div>
+              <div className="page-content">
+                Bu özellik sayesinde toplanan datalar daha çabuk ve kesin sonuçlar verecek şekilde
+                işlenir.
+              </div>
+              <div className="page-title">Tek Tıklama ile Dışa Aktarım</div>
+              <div className="page-content">
+                Bu özellik ile taranan bölge datalarını dışa aktarmak ve değiştirmek çok daha hızlı
+                ve kolay hale gelmektedir.
+              </div>
+
+              <div className="page-title">Dokunmatik Ekranın Sihri</div>
+              <div className="page-content">
+                Geliştirilmiş dokunmatik ekran navigasyonu ,sezgisel bir arayüz yazılımı ve
+                kullanıcı arasında direkt bir etkileşim sağlar.
+              </div>
+              <div className="page-title">Hybrid Tarama</div>
+              <div className="page-content">
+                Hybrid tarama özelliği ile zorlu implant ve restorative tedavi verilerinin
+                yakalanmasında ve implant kaynaklı restoratif tedavilerin datalarının kolaylıkla
+                alınmasını sağlar.
               </div>
               {/* ... */}
             </Grid>
