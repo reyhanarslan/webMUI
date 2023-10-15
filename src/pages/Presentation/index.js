7b0ab303646e1cc82fa0b798e3673670336d4e67
ryhnslan <ryhnslan@gmail.com>
2023-10-15 22:44:05 +0300

    home page tested

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -17,32 +17,69 @@ import { CardMedia } from "@mui/material";
 import routes from "routes";
 
 // import bgImage from "assets/images/dashboard/perfect-healthy-teeth-smile-young-woman.jpg";
-import bgImage1 from "assets/images/dashboard/test.jpg";
+// import bgImage1 from "assets/images/dashboard/test.jpg";
 import CarouselImages from "./sections/Carousel";
 import intro from "assets/videos/UP3D.mp4";
 import ReactPlayer from "react-player";
-import { useEffect } from "react";
+import sld1 from "assets/images/carousel/SLIDER-1psd.jpg";
+import sld2 from "assets/images/carousel/SLIDER-2.jpg";
+import sld3 from "assets/images/carousel/SLIDER-3.jpg";
+import React, { useState, useEffect, useRef } from "react";
 
 import("./video.css");
 
 function Presentation() {
+  const [images] = useState([sld1, sld2, sld3]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(true);
+  const playerRef = useRef(null);
+
+  const imageDuration = 30000;
+
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const bgPosition = `-${scrollY / 15}px 0`;
-      const divBackground = document.querySelector(".div-background");
-
-      if (divBackground) {
-        divBackground.style.backgroundPosition = bgPosition;
+    const interval = setInterval(() => {
+      if (isVideoPlaying) {
+        if (playerRef.current) {
+          const player = playerRef.current;
+          if (player && player.getCurrentTime) {
+            const currentTime = player.getCurrentTime();
+            if (currentTime >= player.getDuration()) {
+              setIsVideoPlaying(false);
+              setCurrentIndex(0);
+              setTimeout(() => {
+                setIsVideoPlaying(true);
+              }, imageDuration);
+            }
+          }
+        }
+      } else {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setTimeout(() => {
+          setIsVideoPlaying(true);
+        }, imageDuration);
       }
-    };
+    }, 10000);
 
-    window.addEventListener("scroll", handleScroll);
+    return () => clearInterval(interval);
+  }, [currentIndex, isVideoPlaying]);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollY = window.scrollY;
+  //     const bgPosition = `-${scrollY / 15}px 0`;
+  //     const divBackground = document.querySelector(".div-background");
+
+  //     if (divBackground) {
+  //       divBackground.style.backgroundPosition = bgPosition;
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
   return (
     <>
       <DefaultNavbar
@@ -68,66 +105,56 @@ function Presentation() {
           <Grid container>
             <Grid className="d-flex" item xs={12} md={6} lg={3} sx={{ ml: 0, mb: 3 }}>
               {" "}
-              {/* marginLeft'u 0 olarak ayarladık */}
               <CardMedia style={{ width: "302px", height: "77px" }} image={logo} alt="Logo" />
             </Grid>
           </Grid>
         </MKBox>
         <Card>
-          <MKBox
+          {/* <MKBox
             minHeight="90vh"
             width="100%"
             sx={{
               padding: 2,
-              backgroundImage: `url(${bgImage1})`, //video gelecek
+              // backgroundImage: `url(${bgImage1})`, //video gelecek
               backgroundSize: "cover",
               backgroundPosition: "top",
               display: "grid",
               placeItems: "center",
             }}
-          >
-            {/* <CardMedia
-            <DefaultNavbar
-              routes={routes}
-              action={{
-                type: "external",
-                route:
-                  "https://api.whatsapp.com/send?phone=+905078688953&text=Merhabasiparivermekistiyorum",
-                label: "Whatsapp ıle Sipariş ",
-                color: "success",
-              }}
-              sticky
-            />
-          </Grid>
-        </Grid>
-      </MKBox>
-      <Card
-        style={{
-          marginRight: "100px",
-          marginLeft: "100px",
-        }}
-      >
-        <MKBox
-          minHeight="90vh"
-          width="100%"
-          sx={{
-            backgroundSize: "cover",
-            backgroundPosition: "top",
-            display: "grid",
-            placeItems: "center",
-          }}
-        >
-          {/* <CardMedia
-          width={300}
-          height={300}
-          component="video"
-          src="video"
-          // className={classes.media}
-          image={intro}
-          autoPlay
-        /> */}
+          > */}
+
+          {isVideoPlaying ? (
             <div className="d-flex justify-content-center align-items-center video-card">
               <ReactPlayer
+                ref={playerRef}
+                style={{ opacity: "0.90" }}
+                width="100%"
+                height="100%"
+                url={intro}
+                playing={true}
+                muted={true}
+                controls={false}
+          
+              />
+            </div>
+          ) : (
+            <div
+              className={`d-flex justify-content-center align-items-center custom-carousel ${
+                isVideoPlaying ? "" : "active"
+              }`}
+            >
+              <img
+                width="100%"
+                height="700px"
+                src={images[currentIndex]}
+                className="image-transition"
+              />
+            </div>
+          )}
+
+          {/* </MKBox> */}
+        </Card>{" "}
+        {/* <ReactPlayer
                 style={{ opacity: "0.90" }}
                 width={"100%"}
                 height="100%"
@@ -135,10 +162,7 @@ function Presentation() {
                 playing={true}
                 muted={true}
                 controls={true}
-              />
-            </div>
-          </MKBox>
-        </Card>
+              /> */}
         <Card
           // className="div-background"
           sx={{
