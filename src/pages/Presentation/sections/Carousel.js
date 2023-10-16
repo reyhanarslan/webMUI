419ea40d574f2df07f02094f192efef8415d9890
ryhnslan <ryhnslan@gmail.com>
2023-10-17 00:05:56 +0300

    home page taslak?

--- src/pages/Presentation/sections/Carousel.js
+++ src/pages/Presentation/sections/Carousel.js
@@ -14,41 +14,131 @@ import sld2 from "assets/images/carousel/SLIDER-2.jpg";
 import sld3 from "assets/images/carousel/SLIDER-3.jpg";
 
 import { useState } from "react";
-
+import { useCallback } from "react";
+import { loadFull } from "tsparticles";
+import Particles from "react-particles";
 import { Grid } from "@mui/material";
 function CarouselImages() {
   const [images] = useState([sld1, sld2, sld3]);
+  const options = {
+    // background: {
+    //   color: {
+    //     value: "#0d47a1",
+    //   },
+    // },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "slow",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 1,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: ["#4A90E2", "#6BB9F0", "#AFCBF4", "#D1E4F7"],
+      },
+      links: {
+        color: "#D1E4F7",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 400,
+        },
+        value: 20,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    detectRetina: true,
+  };
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
   return (
-    <MKBox
-      sx={{
-        background: "#fffff",
-        borderRadius: "30px",
-      }}
-      py={12}
-    >
-      <Container>
-        <Grid lg={3}></Grid>
-        <Grid className="d-flex justify-content-center align-items-center" lg={3}>
-          <Carousel
-            animation="fade"
-            duration={1500}
-            height={"600px"}
-            navButtonsAlwaysVisible={false}
-            fullHeightHover={true}
-            stopAutoPlayOnHover={true}
-            swipe={false}
-            className="custom-carousel"
+    <>
+      {" "}
+      <div
+        style={{
+          background: "linear-gradient(90deg, rgba(188, 188, 189, 0), #11304C 20.71%)",
 
-            // style={{ width: "1920px" }}
-          >
-            {images.map((item, i) => (
-              <img key={i} src={item} />
-            ))}
-          </Carousel>
-        </Grid>
-        <Grid lg={3}></Grid>
-      </Container>
-    </MKBox>
+          // background: "linear-gradient(90deg, rgba(188, 188, 189, 0), rgba(7, 7, 67, 0.8) 25.71%)",
+        }}
+      >
+        <Particles
+          options={options}
+          init={particlesInit}
+          className="custom-particles" // Özel bir CSS sınıf ekleyin
+        />
+        <MKBox
+          sx={{
+            background: "#fffff",
+            borderRadius: "30px",
+          }}
+          py={6}
+        >
+          <Container>
+            <Grid lg={3}></Grid>
+            <Grid className="d-flex justify-content-center align-items-center" lg={3}>
+              <Carousel
+                animation="fade"
+                duration={1500}
+                height={"600px"}
+                navButtonsAlwaysVisible={false}
+                fullHeightHover={true}
+                stopAutoPlayOnHover={true}
+                swipe={false}
+                className="custom-carousel"
+
+                // style={{ width: "1920px" }}
+              >
+                {images.map((item, i) => (
+                  <img key={i} src={item} />
+                ))}
+              </Carousel>
+            </Grid>
+            <Grid lg={3}></Grid>
+          </Container>
+        </MKBox>{" "}
+      </div>
+    </>
   );
 }
 
--- src/pages/Presentation/sections/Pages.js
+++ src/pages/Presentation/sections/Pages.js
@@ -1,5 +1,4 @@
 // react-router-dom components
-import { Link } from "react-router-dom";
 
 // @mui material components
 import Container from "@mui/material/Container";
@@ -9,77 +8,12 @@ import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 
 // Presentation page components
-import ExampleCard from "pages/Presentation/components/ExampleCard";
 import Icon from "@mui/material/Icon";
 import DashboardImg from "assets/images/dashboard/arena-dis-banner-2-2.jpg";
 
-// Data
-import data from "pages/Presentation/sections/data/pagesData";
-
 function Pages() {
-  const renderData = data.map(({ image, name, route }) => (
-    <Grid item xs={12} lg={4} md={6} sx={{ mb: { xs: 3, lg: 0 } }} key={name}>
-      <Link to={route}>
-        <ExampleCard image={image} name={name} display="grid" minHeight="auto" />
-      </Link>
-    </Grid>
-  ));
-
   return (
     <>
-      <MKBox sx={{ background: "#F8F9FF" }} component="section">
-        <Container>
-          <Grid
-            container
-            item
-            xs={12}
-            lg={6}
-            flexDirection="column"
-            alignItems="center"
-            sx={{ textAlign: "center", my: 3, mx: "auto" }}
-          >
-            {/* <MKBadge
-            variant="contained"
-            color="info"
-            badgeContent="boost creativity"
-            container
-            sx={{ mb: 2 }}
-          /> */}
-            <MKTypography variant="h2" fontWeight="bold">
-              Ürünlerimiz
-            </MKTypography>
-            <MKTypography variant="body1" color="text">
-              Ürünlerimiz sektörün ihtiyaçlarına uygun çözümler sunuyor.
-            </MKTypography>
-          </Grid>
-        </Container>
-        <Container sx={{ mt: { xs: 8, lg: 16 } }}>
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              lg={16}
-              sx={{ px: { xs: 0, lg: 8 } }}
-              style={{ marginLeft: "50px", marginRight: "50px" }}
-            >
-              <Grid container spacing={5}>
-                {renderData}
-              </Grid>
-            </Grid>
-            {/* <Grid item xs={12} lg={3}>
-            <MKBox position="sticky" top="100px" pb={{ xs: 2, lg: 6 }}>
-              <MKTypography variant="h3" fontWeight="bold" mb={1}>
-                Presentation Pages for Company, Landing Pages, Blogs and Support
-              </MKTypography>
-              <MKTypography variant="body2" fontWeight="regular" color="secondary" mb={1} pr={2}>
-                These is just a small selection of the multiple possibitilies you have. Focus on the
-                business, not on the design.
-              </MKTypography>
-            </MKBox>
-          </Grid> */}
-          </Grid>
-        </Container>
-      </MKBox>
       <MKBox
         display="flex"
         alignItems="center"
@@ -87,25 +21,19 @@ function Pages() {
         my={2}
         py={6}
         sx={{
+          borderRadius: "30px",
+
           backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
             `${linearGradient(
-              rgba(gradients.dark.main, 0.3),
-              rgba(gradients.dark.state, 0.9)
+              rgba(gradients.light.main, 0.3),
+              rgba(gradients.arena.state, 0.9)
             )}, url(${DashboardImg})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
         <Container>
-          <Grid container item xs={12} lg={6} sx={{ ml: { xs: 0, lg: 6 } }}>
-            {/* <MKTypography variant="h4" color="white" fontWeight="bold">
-            DAHA FAZLA ÜRÜN BİLGİSİ İÇİN <br />
-          </MKTypography> */}
-            {/* <MKTypography variant="body1" color="white" opacity={0.8} mb={2}>
-            From colors, cards, typography to complex elements, you will find the full
-            documentation. Play with the utility classes and you will create unlimited combinations
-            for our components.
-          </MKTypography> */}
+          <Grid container item xs={6} lg={6} sx={{ ml: { xs: 0, lg: 6 } }}>
             <MKTypography
               component="a"
               href="/urunler/cad-cam-sistemleri/kaziyicilar"
@@ -117,13 +45,11 @@ function Pages() {
               sx={{
                 display: "flex",
                 alignItems: "center",
-
                 "& .material-icons-round": {
                   fontSize: "1.125rem",
                   transform: `translateX(3px)`,
                   transition: "transform 0.2s cubic-bezier(0.34, 1.61, 0.7, 1.3)",
                 },
-
                 "&:hover .material-icons-round, &:focus .material-icons-round": {
                   transform: `translateX(6px)`,
                 },
